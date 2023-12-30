c4e268c37bc3af41396b62062ff096b162c8091c
Manoranjan A <ranjanmano10@gmail.com>
2023-12-30 18:38:42 +0530

    remote push commit

---
+++ OpenWeather/View/PicALocView.swift
@@ -0,0 +1,20 @@
+//
+//  PicALocView.swift
+//  OpenWeather
+//
+//  Created by Mano on 29/12/23.
+//
+
+import SwiftUI
+
+struct PicALocView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PicALocView_Previews: PreviewProvider {
+    static var previews: some View {
+        PicALocView()
+    }
+}
